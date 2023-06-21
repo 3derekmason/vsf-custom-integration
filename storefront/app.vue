cd90a464ec41f1139cd88d44b9702a1b4bd04862
3derekmason <3derekmason@gmail.com>
2023-06-21 08:54:42 -0600

    chore: add tailwind/vsf ui

--- storefront/app.vue
+++ storefront/app.vue
@@ -5,6 +5,10 @@
 </template>
 
 <style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
 body {
   margin: 0;
   padding: 0;
--- storefront/nuxt.config.ts
+++ storefront/nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      apiBase: process.env.BASE_URL || 'http://localhost:8181/medusa',
-    },
-  },
+  modules: ['@nuxtjs/tailwindcss'],
 });
